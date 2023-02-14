df3f8d6061eef114fadcac0b5920c150bd536d80
theo3 <arisstephenson2@gmail.com>
2023-02-14 12:37:42 -0800

    add loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:zphos/app.dart';
 import 'package:zphos/firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'package:zphos/services/post_provider.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  setPathUrlStrategy();
   runApp(
     MultiProvider(
       providers: [
